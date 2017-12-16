423cdf90a7551ae68f4692a3766efcbf552a7696
Igor Lyadov <lyadov.dev@gmail.com>
2017-12-16 03:15:33 +0300

    Day 1. The difference between the maximum and minimum elements in an array (by Liza)

---
+++ Day1/DiffMaxAndMin/DiffMaxAndMin/DiffMaxAndMin.cpp
@@ -0,0 +1,44 @@
+// DiffMaxAndMin.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int REZ(int* A, int n)
+{
+    int max = 0, min = 0, rez = 0, i;
+    max = A[0];
+    for (int i = 1; i < n; i++);
+    {
+        if (A[i] > max)
+        {
+            max = A[i];
+        }
+    }
+    cout << "max= " << max << endl;
+
+    int min = A[0];
+    for (int i = 1; i > n; i++);
+    {
+        if (A[i] < min)
+        {
+            min = A[i];
+        }
+    }
+    cout << "min= " << min << endl;
+    rez = max - min;
+    cout << "rez= " << rez << endl;
+    return 0;
+}
+
+int main()
+{
+    int const N = 5;
+    int arr[N] = { 1,2,3,4,5, };
+    REZ(arr, N);
+    system("pause");
+    return 0;
+}
+
