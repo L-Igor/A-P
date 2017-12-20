7087bd94536893c3025ddfafb6d16042158d05ca
Igor Lyadov <lyadov.dev@gmail.com>
2017-12-20 11:34:23 +0300

    День 2. Разница между четными и нечетными позициями в массиве - исправлено.

--- Day2/DiffEvenAndOddPositions/DiffEvenAndOddPositions/DiffEvenAndOddPositions.cpp
+++ Day2/DiffEvenAndOddPositions/DiffEvenAndOddPositions/DiffEvenAndOddPositions.cpp
@@ -3,30 +3,39 @@
 
 #include "stdafx.h"
 #include <iostream>
+
 using namespace std;
+
+
+//разница между четными и нечетными позициями в массиве(2)
 int REZ(int* A, int N)
 {
     int CH = 0;
     int NECH = 0;
     int rez = 0;
-    for (int i = 0; i < N; i + 2)
+
+    for (int i = 0; i < N; i += 2)
     {
-        CH = CH + A[i];
+        CH = CH + A[i];         // CH += A[i];
     }
-    for (int i = 1; i < N; i + 2)
+
+    for (int i = 1; i < N; i += 2)
     {
-        NECH = NECH + A[i];
+        NECH = NECH + A[i];     // NECH += A[i];
     }
+
     rez = CH - NECH;
-    cout << "REZ= " << rez << endl;
-    return 0;
+
+    return rez;     // return CH - NECH;
 }
 
 int main()
 {
     int const n = 5;
-    int a[n] = { 2,4,6,7,8 };
-    REZ(a, n);
+    int a[n] = { 2, 4, 6, 7, 8 };
+    int rez = REZ(a, n);
+    cout << "REZ = " << rez << endl;
+
     system("pause");
     return 0;
 }
